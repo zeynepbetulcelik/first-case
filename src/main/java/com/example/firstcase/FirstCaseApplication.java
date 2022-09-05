489fc53d7ab6bf621794b60efbabf863073654f1
zeynep betül çelik <zeynebbettul@gmail.com>
2022-09-06 00:03:58 +0300

    Initial commit

---
+++ src/main/java/com/example/firstcase/FirstCaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.firstcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstCaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstCaseApplication.class, args);
+	}
+
+}
